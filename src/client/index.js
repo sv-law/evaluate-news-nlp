0b29a9d26c5ffedfc69b1bf3b36311a6087bfcfd
sv-law <svasti.law0@gmail.com>
2023-11-07 01:12:22 +1100

    Add import scss styles statements

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+// Import styles
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
-alert("I EXIST")
-console.log("CHANGE!!");
+console.log(checkForName);
+// alert("I EXIST")
+// console.log("CHANGE!!");
