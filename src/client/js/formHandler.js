50b47c992e7c38e51e99e18f074eab9dfd09790c
sv-law <svasti.law0@gmail.com>
2023-11-07 18:16:42 +1100

    port 8081, return json response, insert data.message

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,10 +6,12 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    fetch('http://localhost:8081/test')
+    .then(res => {
+        return res.json()
+    })
+    .then(function(data) {
+        document.getElementById('results').innerHTML = data.message
     })
 }
 
