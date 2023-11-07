f98d6718df20d397c1b348212396eee229e4d5da
sv-law <svasti.law0@gmail.com>
2023-11-07 17:59:31 +1100

    Export functions, checkForName and handleSubmit

--- src/client/index.js
+++ src/client/index.js
@@ -8,6 +8,11 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
+export {
+    checkForName,
+    handleSubmit
+}
+
 console.log(checkForName);
 // alert("I EXIST")
 // console.log("CHANGE!!");
