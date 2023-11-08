6db1ff527866c373f97dd5409f15798a875bbe99
sv-law <svasti.law0@gmail.com>
2023-11-08 14:41:23 +1100

    Report the concealed API key via process.env.API_KEY to server console

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -9,6 +12,8 @@ app.use(cors())
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+console.log(`API key: ${process.env.API_KEY}`);
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
